4566243886a055203fb84b85640fdf3588758b3c
Vincent Deloso <mitsugaru@gmail.com>
2012-04-09 13:08:48 -0400

    Updated colorize method.

--- KarmicJail/src/com/mitsugaru/karmicjail/KarmicJail.java
+++ KarmicJail/src/com/mitsugaru/karmicjail/KarmicJail.java
@@ -34,7 +34,6 @@ import org.bukkit.plugin.java.JavaPlugin;
 import com.platymuus.bukkit.permissions.Group;
 import com.platymuus.bukkit.permissions.PermissionsPlugin;
 
-// TODO reason after being jailed
 public class KarmicJail extends JavaPlugin
 {
 	// Class Variables
@@ -2001,19 +2000,31 @@ public class KarmicJail extends JavaPlugin
 	/**
 	 * Colorizes a given string to Bukkit standards
 	 * 
+	 * http://forums.bukkit.org/threads/methode-to-colorize.69543/#post-1063437
+	 * 
 	 * @param string
 	 * @return String with appropriate Bukkit ChatColor in them
-	 * @author Coryf88
+	 * @author AmberK
 	 */
 	public String colorizeText(String string)
 	{
-		// FIXME ILLEGAL FORMAT CONVERSION EXCEPTION x != java.lang.Character
-		/*
-		 * for (ChatColor color : ChatColor.values()) { string =
-		 * string.replace(String.format("&%x", color.getChar()),
-		 * color.toString()); }
-		 */
-		return string;
+		string = string.replaceAll("&0", ""+ChatColor.BLACK);
+	    string = string.replaceAll("&1", ""+ChatColor.DARK_BLUE);
+	    string = string.replaceAll("&2", ""+ChatColor.DARK_GREEN);
+	    string = string.replaceAll("&3", ""+ChatColor.DARK_AQUA);
+	    string = string.replaceAll("&4", ""+ChatColor.DARK_RED);
+	    string = string.replaceAll("&5", ""+ChatColor.DARK_PURPLE);
+	    string = string.replaceAll("&6", ""+ChatColor.GOLD);
+	    string = string.replaceAll("&7", ""+ChatColor.GRAY);
+	    string = string.replaceAll("&8", ""+ChatColor.DARK_GRAY);
+	    string = string.replaceAll("&9", ""+ChatColor.BLUE);
+	    string = string.replaceAll("&a", ""+ChatColor.GREEN);
+	    string = string.replaceAll("&b", ""+ChatColor.AQUA);
+	    string = string.replaceAll("&c", ""+ChatColor.RED);
+	    string = string.replaceAll("&d", ""+ChatColor.LIGHT_PURPLE);
+	    string = string.replaceAll("&e", ""+ChatColor.YELLOW);
+	    string = string.replaceAll("&f", ""+ChatColor.WHITE);
+	    return string;
 	}
 
 	public enum JailStatus
